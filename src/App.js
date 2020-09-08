178cb6abc1329e56356a5515dcc27ad18895ea0b
Michael Brattin <mbrattin@dmp.com>
2020-09-08 16:49:04 -0500

    add tiny clock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { useState } from "react";
 import "./App.css";
-import Minus from "./minus.js";
-import Plus from "./plus.js";
 import Counter from "./Counter.js";
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  const updateInterval = setInterval(() => setTime(new Date()), 500);
+
+  const getMonthFromNumber = (num) => {
+    switch (num) {
+      case 1:
+        return "January";
+      case 2:
+        return "February";
+      case 3:
+        return "March";
+      case 4:
+        return "April";
+      case 5:
+        return "May";
+      case 6:
+        return "June";
+      case 7:
+        return "July";
+      case 8:
+        return "August";
+      case 9:
+        return "September";
+      case 10:
+        return "October";
+      case 11:
+        return "November";
+      case 12:
+        return "December";
+    }
+  };
+
+  return (
+    <div>{`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} ${getMonthFromNumber(
+      time.getMonth() + 1
+    )} ${time.getDate()} ${time.getFullYear()}`}</div>
+  );
+};
 
 function App() {
   return (
     <>
       <Counter />
+      <Clock />
     </>
   );
 }
